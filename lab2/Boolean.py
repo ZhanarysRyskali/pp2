1ccc0954e8b60b8548b333624fad0fba91c07506
ZhanarysRyskali <156776852+ZhanarysRyskali@users.noreply.github.com>
2024-01-24 12:39:28 +0600

    Lab2

---
+++ lab2/Boolean.py
@@ -0,0 +1,19 @@
+#ex 1
+print(10>9)
+#This statement will return True
+
+#ex 2
+print(10 == 9)
+#This statement will return False
+
+#ex 3
+print(10<9)
+#This statement will return False
+
+#ex 4
+print(bool("abc"))
+#If a statement has bool(smth) it will return True unless smth equals 0 or it is just bool()
+
+#ex 5
+print(bool(0))
+#It will return False
---
+++ lab2/Lists.py
@@ -0,0 +1,34 @@
+#ex 1
+fruits = ["apple", "banana", "cherry"]
+print(fruits[0])
+
+#ex 2
+fruits = ["apple", "banana", "cherry"]
+fruits[0] = "kiwi"
+print(fruits)
+
+#ex 3
+fruits = ["apple", "banana", "cherry"]
+fruits.append("orange")
+
+#ex 4
+fruits = ["apple", "banana", "cherry"]
+fruits.insert(1, "lemon")
+print(fruits)
+
+#ex 5
+fruits = ["apple", "banana", "cherry"]
+fruits.remove("banana")
+print(fruits)
+
+#ex 6
+fruits = ["apple", "banana", "cherry"]
+print(fruits[-1])
+
+#ex 7
+fruits = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(fruits[2:5])
+
+#ex 8
+fruits = ["apple", "banana", "cherry"]
+print(len(fruits))
